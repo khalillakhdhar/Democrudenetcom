29c262e390cec00bee23bd742b1f1766271cffad
khalil <khalillakhdharatc@gmail.com>
2022-03-13 11:43:54 +0100

    code

---
+++ src/main/java/com/mycompany/democrud/UserModel.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.democrud;
+
+/**
+ *
+ * @author khali
+ */
+public class UserModel {
+    private int id;
+    private String nom;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public UserModel(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public UserModel() {
+    }
+    
+    
+    
+}
