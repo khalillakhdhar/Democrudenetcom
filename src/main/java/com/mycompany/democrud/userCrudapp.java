86bcabf9ffbb152eefd31bf4d9d4e15a5adde4d3
khalil <khalillakhdharatc@gmail.com>
2022-03-23 17:39:16 +0100

    demo

--- src/main/java/com/mycompany/democrud/userCrudapp.java
+++ src/main/java/com/mycompany/democrud/userCrudapp.java
@@ -32,11 +32,11 @@ public class userCrudapp extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 614, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 391, Short.MAX_VALUE)
         );
 
         pack();
